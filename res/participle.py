85d5529af43dffedc659b3ae1716e8fc09b23fc0
wuxiec <Magnesiumwuxiec@163.com>
2023-05-04 23:22:40 +0800

    完善

--- res/participle.py
+++ res/participle.py
@@ -1,27 +1,21 @@
 def chinese_word_cut(mytext):
     jieba.load_userdict(dic_file)
     jieba.initialize()
-    try:
-        stopword_list = open(stop_file,encoding ='utf-8')
-    except:
-        stopword_list = []
-        print("error in stop_file")
+    stopword_list = open(stop_file,encoding ='utf-8')
     stop_list = []
     flag_list = ['n','nz','vn']
     for line in stopword_list:
         line = re.sub(u'\n|\\r', '', line)
         stop_list.append(line)
-    
     word_list = []
-    #jieba分词
     seg_list = psg.cut(mytext)
     for seg_word in seg_list:
         word = re.sub(u'[^\u4e00-\u9fa5]','',seg_word.word)
         find = 0
         for stop_word in stop_list:
             if stop_word == word or len(word)<2:
-                    find = 1
-                    break
+                find = 1
+                break
         if find == 0 and seg_word.flag in flag_list:
             word_list.append(word)      
     return (" ").join(word_list)
